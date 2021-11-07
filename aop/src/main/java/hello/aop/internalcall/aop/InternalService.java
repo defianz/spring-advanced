d0f51ffc1e593740f5f9c4251af33b53cbc00f93
Defian <iautm91@gmail.com>
2021-11-07 19:26:37 +0900

    스프링 AOP - 실무 주의 사항

---
+++ aop/src/main/java/hello/aop/internalcall/aop/InternalService.java
@@ -0,0 +1,4 @@
+package hello.aop.internalcall.aop;
+
+public class InternalService {
+}
---
+++ aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,41 @@
+package hello.aop.proxyvs;
+
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+@Slf4j
+public class ProxyCastingTest {
+
+
+    @Test
+    public void jdkProxy() throws Exception {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false);
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        // JDK 동적 프록시를 구현 클래스로 캐스팅 시도 실패 , ClassCastException 예외 발생
+        Assertions.assertThrows(ClassCastException.class, () ->
+        {MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;}
+        );
+    }
+
+    @Test
+    public void cglibProxy() throws Exception {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        log.info("proxy Class ={} ", memberServiceProxy.getClass());
+
+        // JDK 동적 프록시를 구현 클래스로 캐스팅 시도 실패 , ClassCastException 예외 발생
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+    }
+
+}
